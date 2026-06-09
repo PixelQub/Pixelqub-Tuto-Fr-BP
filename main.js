1e979cef1e22986d9952527dbc06867114e3471e
PixelQub-staff <ctephanz.box@gmail.com>
2026-06-08 22:23:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import { world } from "@minecraft/server";
+
+world.sendMessage("Hello World depuis mon script Bedrock !");
+
+
+/* import { ItemStack, EquipmentSlot, GameMode, world } from '@minecraft/server'; */
