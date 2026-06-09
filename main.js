3379f4626c5f276f1c6bb93cc8da7e29a8543c82
PixelQub-staff <ctephanz.box@gmail.com>
2026-06-08 22:26:32 -0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import { world } from "@minecraft/server";
-
-world.sendMessage("Hello World depuis mon script Bedrock !");
-
-
-/* import { ItemStack, EquipmentSlot, GameMode, world } from '@minecraft/server'; */
